6f4fd47a22b8f9e9f81dedea4dc3acbadf377033
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-06-26 13:51:26 -0500

    make dark mode enabled by default

--- frontend/src/components/themeToggle.jsx
+++ frontend/src/components/themeToggle.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as MoonIcon } from "../assets/moon.svg";
 import { ReactComponent as SunIcon } from "../assets/sun.svg";
 
 export const ThemeToggle = () => {
-  const [isEnabled, setIsEnabled] = useState(false) 
+  const [isEnabled, setIsEnabled] = useState(true) 
   const initialStyles = useRef({});
 
   useEffect(() => {
