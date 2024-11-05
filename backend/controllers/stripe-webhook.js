97fe1e2bc42d3fb3e5d4cfffdf5a99d4003707e0
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-11-05 13:47:14 -0600

    redefine customer variables before saving ot database

--- backend/controllers/stripe-webhook.js
+++ backend/controllers/stripe-webhook.js
@@ -17,65 +17,55 @@ router.post('/', async (req, res) => {
         return
       }
     
-    // switch (event.type) {
-    //     case 'checkout.session.completed':
-    //         const checkoutSessionCompleted = event.data.object
+    switch (event.type) {
+        case 'checkout.session.completed':
+            const checkoutSessionCompleted = event.data.object
             
-    //         const customerName = session.customer_details.name
-    //         const customerEmail = session.customer_details.email
-    //         const orderDate = new Date(session.created * 1000)
-    //         const totalAmount = session.amount_total / 100
+            // const customerName = session.customer_details.name
+            // const customerEmail = session.customer_details.email
+            // const orderDate = new Date(session.created * 1000)
+            // const totalAmount = session.amount_total / 100
+            console.log('event.data.object', event.data.object)
 
-    //         try {
-    //           // Save to orders table
-    //           const newOrder = await Order.create({
-    //             order_date: orderDate,
-    //             customer_name: customerName,
-    //             customer_email: customerEmail,
-    //             total_amount: totalAmount
-    //           })
+            // try {
+            //   // Save to orders table
+            //   const newOrder = await Order.create({
+            //     order_date: orderDate,
+            //     customer_name: customerName,
+            //     customer_email: customerEmail,
+            //     total_amount: totalAmount
+            //   })
         
-    //           // Retrieve line items from the session to save in order_details table
-    //           const lineItems = await stripe.checkout.sessions.listLineItems(session.id)
+            //   // Retrieve line items from the session to save in order_details table
+            //   const lineItems = await stripe.checkout.sessions.listLineItems(session.id)
         
-    //           // Save each item in order_details table
-    //           // for (const item of lineItems.data) {
+            //   // Save each item in order_details table
+            //   // for (const item of lineItems.data) {
 
-    //           // const product = await Product.findOne({
-    //           //   where: { name: productName, size: size }
-    //           // })
-    //           //   await OrderDetail.create({
-    //           //     order_id: newOrder.id,
-    //           //     product_id: item.price.product,
-    //           //     quantity: item.quantity,
-    //           //     unit_price: item.price.unit_amount / 100
-    //           //   })
-    //           // }
+            //   // const product = await Product.findOne({
+            //   //   where: { name: productName, size: size }
+            //   // })
+            //   //   await OrderDetail.create({
+            //   //     order_id: newOrder.id,
+            //   //     product_id: item.price.product,
+            //   //     quantity: item.quantity,
+            //   //     unit_price: item.price.unit_amount / 100
+            //   //   })
+            //   // }
 
-    //           console.log(newOrder, 'newOrder')
-    //           console.log(lineItems, 'lineItems')
+            //   console.log(newOrder, 'newOrder')
+            //   console.log(lineItems, 'lineItems')
         
-    //           res.status(200).send()
-    //         } catch (error) {
-    //           console.error('Error saving order to database:', error)
-    //           res.status(500).send('Internal Server Error')
-    //         }
+            //   res.status(200).send()
+            // } catch (error) {
+            //   console.error('Error saving order to database:', error)
+            //   res.status(500).send('Internal Server Error')
+            // }
 
-    //         break
-    //     case 'payment_intent.payment_failed':
-    //         const paymentIntentPaymentFailed = event.data.object;
-    //         // Then define and call a function to handle the event payment_intent.payment_failed
-    //         break
-    //     case 'payment_intent.succeeded':
-    //         const paymentIntentSucceeded = event.data.object;
-    //         // Then define and call a function to handle the event payment_intent.succeeded
-    //         break
-    //     // ... handle other event types
-    //     default:
-    //         console.log(`Unhandled event type ${event.type}`)
-    // }
-    
-      console.log(`Unhandled event type ${event.type}`)
+            break
+        default:
+            console.log(`Unhandled event type ${event.type}`)
+    }
     
       // Return a 200 response to acknowledge receipt of the event
       res.send()
