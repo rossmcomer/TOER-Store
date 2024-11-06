7e9b74e8d14a73f319bbc90423af5c755dbaa266
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-11-06 17:37:12 -0600

    create order-details controller for fetching users order details

---
+++ backend/controllers/order-details.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const { Product, ProductImage, Category, Supplier, Order, OrderDetail } = require('../models')
+
+router.get('/', async (req, res) => {
+})
+
+module.exports = router
