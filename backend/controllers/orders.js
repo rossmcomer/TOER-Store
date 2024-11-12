e1624bd7907e8b4e204a49dc00a6d7133b6f3144
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-11-11 18:13:38 -0600

    begin creating endpoint for order details

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
-const { Product, ProductImage, Category, Supplier, Order, OrderDetail } = require('../models')
+const { Product, ProductImage, Category, Order, OrderDetail } = require('../models')
 
 router.get('/', async (req, res) => {
+    const { email } = req.query
+
+    try {
+        const orders = await Order.findAll({
+          where: { customerEmail: email },
+          include: [{ model: OrderDetail, as: 'details' }],
+        })
+    
+        res.json(orders) // Send the result as JSON
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server error' })
+      }
 })
 
 module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 
 const checkoutRouter = require('./controllers/create-checkout-session')
 const productsRouter = require('./controllers/products')
-const ordersRouter = require('./controllers/products')
+const ordersRouter = require('./controllers/orders')
 
 app.use('/api/products', productsRouter)
 app.use('/api/create-checkout-session', checkoutRouter)
