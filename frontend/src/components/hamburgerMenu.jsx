708849016a1f0b8c050ac50cb878be5b462e647d
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-11-20 17:32:48 -0600

    replace hamburger menu with profile pic

--- frontend/src/components/hamburgerMenu.jsx
+++ frontend/src/components/hamburgerMenu.jsx
@@ -2,20 +2,28 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoutButton from './logoutButton'
 import './hamburgerMenu.css'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const { user } = useAuth0()
 
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
   return (
     <div className="hamburgerMenuContainer">
-      <div className="hamburger-icon" onClick={toggleMenu}>
+      {/* <div className="hamburger-icon" onClick={toggleMenu}>
         <span></span>
         <span></span>
         <span></span>
-      </div>
+      </div> */}
+      <img
+        src={user.picture}
+        alt="User Picture"
+        className="profilePic"
+        onClick={toggleMenu}
+      ></img>
       <div className={`menu-options ${isOpen ? 'open' : ''}`}>
         <button className="close-btn" onClick={toggleMenu}>
           X
@@ -24,6 +32,7 @@ const HamburgerMenu = () => {
           <div>Profile</div>
         </Link>
         <LogoutButton />
+        <div className="signed-in">Signed in as {user.name}</div>
       </div>
     </div>
   )
