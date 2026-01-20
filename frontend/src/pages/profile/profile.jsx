a9cd91ba1e57b489153e96277f34cc295da1520a
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2026-01-20 00:30:51 -0600

    remove unused useContext import from profile.jsx

--- frontend/src/pages/profile/profile.jsx
+++ frontend/src/pages/profile/profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import './profile.css'
 import userOrdersService from '../../services/userOrders'
