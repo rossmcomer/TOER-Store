b3484fc99a050435c02aab12bfdbb67d28a24c88
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2026-01-21 00:38:32 -0600

    update cors to use toerstore.com

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 app.use(cors({
-  origin: ['https://toerstore.pages.dev', 'http://localhost:3000']
+  origin: ['https://toerstore.com', 'http://localhost:3000']
 }));
 
 const saveOrderInfoRouter = require('./controllers/stripe-webhook')
